036a6d33ef9343797290bfa119dba5cbcf273d57
Katrin889 <kakamitreva@gmail.com>
2026-01-15 20:11:31 +0200

    Add files via upload

---
+++ zadachi - String/zad 14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word you want to code (only lowercase)");
+            string word = Console.ReadLine();
+            while (word != word.ToLower())
+            {
+                Console.WriteLine("There aren't only lowercase letters!");
+                Console.Write("Enter again: ");
+                word = Console.ReadLine();
+            }
+            //ne moje da ima poveche ot 100 simvola, nqma nujda ot proverka
+            string code = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                string copy = code;
+                code = code.Insert(code.Length, word[i].ToString());
+                code = code.Insert(code.Length, copy);
+            }
+            Console.WriteLine("The word coded is " + code);
+        }
+    }
+}
---
+++ zadachi - String/zad 7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first niz S1:");
+            string s1 = Console.ReadLine();
+            Console.WriteLine("Enter second niz S2: ");
+            string s2 = Console.ReadLine();
+            int br = 0;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s1[i] == s2[0])
+                {
+                    bool ans = true;
+                    for (int j = 0; j < s2.Length; j++)
+                    {
+                        if (s1[i] != s2[j])
+                        {
+                            ans = false;
+                            break;
+                        }
+                        if (i != s1.Length - 1)
+                            i++;
+                    }
+                    if (ans == true)
+                        br++;
+                    i--;
+                }
+            }
+            Console.WriteLine($"Niz S2 repeats {br} in S1");
+        }
+    }
+}
